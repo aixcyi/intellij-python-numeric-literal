270bab588c50a473166e5096ab02f375329940a8
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2024-07-12 11:49:36 +0800

    refactor: 从文档提供者中分离出文档构建器

---
+++ src/main/kotlin/cn/aixcyi/plugin/pnl/ui/NumberLiteralDocumentationProvider.kt
@@ -0,0 +1,40 @@
+package cn.aixcyi.plugin.pnl.ui
+
+import cn.aixcyi.plugin.pnl.utils.IntegerLiteralDocBuilder
+import com.intellij.lang.documentation.AbstractDocumentationProvider
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.jetbrains.python.psi.PyNumericLiteralExpression
+
+/**
+ * 展示数字字面值的文档。
+ *
+ * @author <a href="https://github.com/aixcyi">砹小翼</a>
+ * @see <a href="https://plugins.jetbrains.com/docs/intellij/code-documentation.html">Documentation | IntelliJ Platform Plugin SDK</a>
+ */
+class NumberLiteralDocumentationProvider : AbstractDocumentationProvider() {
+
+    override fun getCustomDocumentationElement(
+        editor: Editor,
+        file: PsiFile,
+        contextElement: PsiElement?,
+        targetOffset: Int
+    ): PsiElement? {
+        val element = contextElement?.parent
+        if (element !is PyNumericLiteralExpression)
+            return null
+        return element
+    }
+
+    override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
+        if (element !is PyNumericLiteralExpression)
+            return null
+        if (element.isIntegerLiteral) {
+            return IntegerLiteralDocBuilder.getInstance(element)?.buildMultiRadixTable()
+        } else {
+            // 未来再决定是否处理小数。
+            return null
+        }
+    }
+}
